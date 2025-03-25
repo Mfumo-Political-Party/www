eddfe8e09bb0f8d22dcaa908aa1f4a869175b8ab
Selaelo2 <selaelolanga10@gmail.com>
2025-03-25 10:18:43 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Users, GraduationCap, Building2, Heart, ChevronDown } from 'lucide-react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import About from './components/About';
-import Policies from './components/Policies';
-import Leadership from './components/Leadership';
-import Footer from './components/Footer';
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import MissionStatement from "./components/MissionStatement";
+import About from "./components/About";
+import Policies from "./components/Policies";
+import Leadership from "./components/Leadership";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="min-h-screen bg-white">
       <Navbar />
       <Hero />
+      <MissionStatement />
       <About />
       <Policies />
       <Leadership />
